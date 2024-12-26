d62f961ec85e0c54a88244791928d9382ba73761
Userkare <Tom_Helmut@yahoo.de>
2024-12-26 19:58:25 +0100

    player can shoot

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 from circleshape import *
 from constants import *
+from Shot import Shot
 
 
 class Player(CircleShape):
@@ -26,6 +27,10 @@ class Player(CircleShape):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
+    def shoot(self):
+        new_shoot = Shot(self.position.x, self.position.y)
+        new_shoot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -38,3 +43,6 @@ class Player(CircleShape):
             self.move(dt)
         if(keys[pygame.K_s]):
             self.move(-dt)
+
+        if(keys[pygame.K_SPACE]):
+            self.shoot()
