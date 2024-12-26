813a95ba540d3a3b5b0736b06d814af2af8d25f5
Userkare <Tom_Helmut@yahoo.de>
2024-12-26 19:25:08 +0100

    player bewegung hinzugefügt

--- main.py
+++ main.py
@@ -6,17 +6,31 @@ from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        for updatable_thing in updatable:
+            updatable_thing.update(dt)
 
-        screen.fill("black")
-        player.draw(screen)
+        screen.fill("black")      
+
+        for drawable_thing in drawable:
+            drawable_thing.draw(screen)
+
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
